ef902d7283dc918b8644b149ce9c0276bb121134
root <root@localhost.localdomain>
2018-04-17 22:51:24 -0400

    changed a code to output standard error. avoiding to dump to standard output...

--- 0213a.rb
+++ 0213a.rb
@@ -1,7 +1,7 @@
-print "devide a integer to 2, pls enter an even number\n"
+STDERR.print "devide a integer to 2, pls enter an even number\n"
 n = gets.to_i
 if n%2 == 0 then
 printf("%d\n", n/2)
 else
-  printf("%d is not a even number.\n", n/2)
+STDERR.printf("%d is not a even number.\n", n)
 end
