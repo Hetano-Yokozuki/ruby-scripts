f1b79170ddbb5e0b8baa18328e322d880412a277
root <root@localhost.localdomain>
2018-04-17 23:23:38 -0400

    a line is string, whole data ia array

--- 0202a.rb
+++ 0202a.rb
@@ -1,6 +1,8 @@
 while line=gets
 data = line.chomp.split(",")
-if /\d+/ =~ data[0]
-printf("%s,%s\n", data[0],data[2])
-end
+# if /\d+/ =~ data[0]
+printf("%s, %s\n", data[0],data[2])
+p data[0].class
+p data.class
+# end
 end
